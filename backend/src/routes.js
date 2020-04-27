8b19e2fde12bb9f5a8abd8d9b5bf0e11700235c7
Thiago Araujo Campos <thiagoaraujocampos@outlook.com>
2020-04-27 02:29:39 -0300

    back-end

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const incidentsController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', incidentsController.index);
+routes.post('/incidents', incidentsController.create);
+routes.delete('/incidents/:id', incidentsController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
