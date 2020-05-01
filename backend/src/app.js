d6a64374841c88538ebcd67264a807ea04136ca6
Thiago Araujo Campos <thiagoaraujocampos@outlook.com>
2020-05-01 14:54:11 -0300

    backend tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -29,5 +30,6 @@ Query Builder: table('users').select('*').where()
 
 app.use(cors());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
